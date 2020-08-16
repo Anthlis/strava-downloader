4b0c1c0647c361d8f266905d2bb79b961f0e173f
Pedro Junqueira <pedrocj@gmail.com>
2020-08-16 21:02:01 +0930

    first commit 🚀

---
+++ app/models.py
@@ -0,0 +1,21 @@
+from app import db
+
+class Athlete(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.Text)
+    lastname = db.Column(db.Text)
+    profile = db.Column(db.Text)
+    profile_medium = db.Column(db.Text)
+    summit = db.Column(db.Boolean)
+    created_at = db.Column(db.DateTime)
+    access_token = db.Column(db.Text)
+    expires_at = db.Column(db.Integer)
+    expires_in = db.Column(db.Integer)
+    refresh_token = db.Column(db.Text)
+    token_type = db.Column(db.Text)
+
+
+    def __repr__(self):
+        return '<Athlete {} - {} >'.format(self.firstname, self.id)
+    
+    
---
+++ app/routes.py
@@ -0,0 +1,61 @@
+import os
+import urllib
+
+from flask import (render_template, 
+                   redirect, 
+                   request, 
+                   flash, 
+                   url_for)
+import requests
+
+from app import app
+
+
+def authorize_url():
+    """Generate authorization uri"""
+    app_url = 'http://127.0.0.1'
+    params = {
+        "client_id": os.getenv('STRAVA_CLIENT_ID'),
+        "response_type": "code",
+        "redirect_uri": f"{app_url}:5000/authorization_successful",
+        "scope": "read,profile:read_all,activity:read",
+        "approval_prompt": "force"
+    }
+    values_url = urllib.parse.urlencode(params)
+    base_url = 'https://www.strava.com/oauth/authorize'
+    auth_url = base_url + '?' + values_url
+    return auth_url
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+@app.route("/authorize")
+def authorize():
+    return redirect(authorize_url())
+
+@app.route("/authorization_successful")
+def authorization_successful():
+    error = request.args.get('error')
+    if error == 'access_denied':
+        flash(f'You denied the required access for Strava Downloader to work')
+
+        return redirect(url_for("index"))
+
+    code = request.args.get('code')
+    scope = request.args.get('scope')
+    params = {
+        "client_id": os.getenv('STRAVA_CLIENT_ID'),
+        "client_secret": os.getenv('STRAVA_CLIENT_SECRET'),
+        "code": code,
+        "grant_type": "authorization_code"
+    }
+    r = requests.post("https://www.strava.com/oauth/token", params)
+    
+    authorization_response = r.json()
+    first_name = authorization_response['athlete']['firstname']
+    flash(f'Hi {first_name} your authorization was successful ')
+
+    #return redirect(url_for("index"))
+    return authorization_response
