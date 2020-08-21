9dfc262b15fd915f9cd5e05f1123a33bf77a0ed0
Pedro Junqueira <pedrocj@gmail.com>
2020-08-21 10:35:38 +0930

    fixed css image path to error pages

--- app/routes.py
+++ app/routes.py
@@ -116,7 +116,7 @@ def download_csv():
     # TODO make router dynamic receiving athlete id and date from webform with validation rules
     # check if token still valid if not refresh token
     # if token revoked or expired logout athlete and return to home page
-    #id = XXXXXX
+    # id = XXXXXXX
     authenticated_athlete = Athlete.query.get(id)
     access_token = authenticated_athlete.access_token
     client = StravaIO(access_token=access_token)
