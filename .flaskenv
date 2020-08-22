9e176da72f4ff672fd2f7388974b3e780be04abe
Pedro Junqueira <pedrocj@gmail.com>
2020-08-22 22:50:20 +0930

    add logging and preparing for Heroku deployment

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,1 @@
 FLASK_APP=strava_downloader.py
-FLASK_DEBUG=1
--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,7 @@
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
+
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -11,4 +15,25 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 
 from app import routes, models, utils, errors
+
+if not app.debug and not app.testing:
+
+
+    if app.config['LOG_TO_STDOUT']:
+            stream_handler = logging.StreamHandler()
+            stream_handler.setLevel(logging.INFO)
+            app.logger.addHandler(stream_handler)
+    else:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/microblog.log',
+                                            maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s '
+            '[in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
 
