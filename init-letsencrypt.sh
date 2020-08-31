930912166124452ece8635e17294ad3439570121
Pedro Junqueira <pedrocj@gmail.com>
2020-09-01 06:53:32 +0930

    change domain on certbot configuration files www.download-data.com

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(download-data.com www.download-data.com)
+domains=(www.download-data.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="pedrocj@gmail.com" # Adding a valid address is strongly recommended
